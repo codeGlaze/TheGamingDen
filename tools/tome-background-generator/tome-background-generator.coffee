cc56ec5b185ee9296126ef228006950e9fe63f6e
codeGlaze <codeGlaze@gmail.com>
2015-03-23 01:17:16 -0400

    initial commit
    I hope I remembered everything >_>

---
+++ tools/tome-background-generator/tome-background-generator.coffee
@@ -0,0 +1,39 @@
+formIn = 
+	bkgd_name : ""
+	bkgd_quote : ""
+	bkgd_effect : ""
+
+format = (id, str) ->
+  if str == "" then str else
+  
+    switch id
+      when "bkgd_name" then  "[b]"+str+":[/b] "
+      when "bkgd_quote" then  "\"[i]"+str+"[/i]\" \n"
+      when "bkgd_effect" then  "\n[b]Effect:[/b] "+str+"\n"
+      else str
+  
+output = ( str ) ->
+  # count number of lines from form
+  numBreaks = (str.match(/\n/g)||[]).length
+  
+  $this = $("#output")
+  # set height of textarea
+  $this.val(str).height(numBreaks*21)
+  return
+  
+go = ( id, str ) ->
+	out = ''
+	for k,v of formIn
+		if id == k then formIn[k] = format id, str
+		console.log "ID:: "+id+"FI: "+formIn[k]+" s: "+str
+		if formIn[k]? and formIn[k] != ""
+      out += formIn[k]
+
+ return output out + "[hr]\n"
+
+# trigger content refresh when textarea changes
+#$(".form-control").on "change input paste keyup",
+#_.throttle go $(@).val()
+
+$("#left .form-control").on "change input paste keyup", ->
+  go $(@).attr("id"), $(@).val()
---
+++ tools/tome-background-generator/tome-background-generator.js
@@ -0,0 +1,54 @@
+(function() {
+  var formIn, format, go, output;
+
+  formIn = {
+    bkgd_name: "",
+    bkgd_quote: "",
+    bkgd_effect: ""
+  };
+
+  format = function(id, str) {
+    if (str === "") {
+      return str;
+    } else {
+      switch (id) {
+        case "bkgd_name":
+          return "[b]" + str + ":[/b] ";
+        case "bkgd_quote":
+          return "\"[i]" + str + "[/i]\" \n";
+        case "bkgd_effect":
+          return "\n[b]Effect:[/b] " + str + "\n";
+        default:
+          return str;
+      }
+    }
+  };
+
+  output = function(str) {
+    var $this, numBreaks;
+    numBreaks = (str.match(/\n/g) || []).length;
+    $this = $("#output");
+    $this.val(str).height(numBreaks * 21);
+  };
+
+  go = function(id, str) {
+    var k, out, v;
+    out = '';
+    for (k in formIn) {
+      v = formIn[k];
+      if (id === k) {
+        formIn[k] = format(id, str);
+      }
+      console.log("ID:: " + id + "FI: " + formIn[k] + " s: " + str);
+      if ((formIn[k] != null) && formIn[k] !== "") {
+        out += formIn[k];
+      }
+    }
+    return output(out + "[hr]\n");
+  };
+
+  $("#left .form-control").on("change input paste keyup", function() {
+    return go($(this).attr("id"), $(this).val());
+  });
+
+}).call(this);
